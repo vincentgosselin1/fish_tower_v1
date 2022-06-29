d4741b20e05bdb302b1f2536d44dedadf324d5aa
unknown <vincentgosselin1@gmail.com>
2022-06-28 21:54:31 -0400

    arduino due

--- ab.bat
+++ ab.bat
@@ -1,1 +1,3 @@
-arduino-cli compile fish_tower_v1.ino --fqbn=arduino:avr:mega:cpu=atmega2560
+::arduino-cli compile fish_tower_v1.ino --fqbn=arduino:avr:mega:cpu=atmega2560
+
+arduino-cli compile fish_tower_v1.ino --fqbn=arduino:sam:arduino_due_x_dbg
