4a242e57f92dfa5f067f6895d04c100aac7c73dc
unknown <vincentgosselin1@gmail.com>
2022-07-27 17:10:00 -0400

    ss

--- fish_tower_v1.ino
+++ fish_tower_v1.ino
@@ -81,7 +81,7 @@ void loop()
 	or Lasertrip3A.get_status() or Lasertrip3B.get_status()
 	or Lasertrip4A.get_status() or Lasertrip4B.get_status()
 	or Lasertrip5A.get_status() or Lasertrip5B.get_status()
-	or PIR_sensor1.get_status()
+	   //or PIR_sensor1.get_status()
          ){
     
     //a fish was detected!
