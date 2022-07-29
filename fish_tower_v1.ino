3116ed6e7e2350737c27b4f74cf26a45f8804e2a
unknown <vincentgosselin1@gmail.com>
2022-07-29 16:41:39 -0400

    pir2

--- fish_tower_v1.ino
+++ fish_tower_v1.ino
@@ -14,6 +14,12 @@ The cameras can also be triggered by 5x PIR sensors.
 
 Version: 1.o
 
+boot up sequence should be
+1. 12V
+2. USB cable
+3. 5V
+4. Camdoo
+
 */
 
 #include "ARDUINO1.h"
@@ -36,9 +42,9 @@ Lasertrip Lasertrip5B(4);
 Camdo Camdo1(35,37); //Camdo #1, pin 35 trigger 3.5mm , pin 37 feedback 2.5mm.
 
 PIR_sensor PIR_sensor1(28);
-//PIR_sensor PIR_sensor2(29);
-//PIR_sensor PIR_sensor3(30);
-//PIR_sensor PIR_sensor4(31);
+PIR_sensor PIR_sensor2(29);
+PIR_sensor PIR_sensor3(30);
+PIR_sensor PIR_sensor4(31);
 
 SmartWait SmartWait1;
 
@@ -49,7 +55,9 @@ void setup()
 
 void loop()
 {
-
+  //turn OFF camdoo
+  Camdo1.stop();
+  
   //listen to lasertrip circuits
   Lasertrip1A.listen();
   Lasertrip1B.listen();
@@ -62,9 +70,7 @@ void loop()
   Lasertrip5A.listen();
   Lasertrip5B.listen();
   PIR_sensor1.listen();
-  Camdo1.stop();
-  
-  //PIR_sensor2.listen();
+  PIR_sensor2.listen();
   //PIR_sensor3.listen();
   //PIR_sensor4.listen();
   
@@ -83,7 +89,7 @@ void loop()
 	or Lasertrip3A.get_status() or Lasertrip3B.get_status()
 	or Lasertrip4A.get_status() or Lasertrip4B.get_status()
 	or Lasertrip5A.get_status() or Lasertrip5B.get_status()
-	   or PIR_sensor1.get_status()
+	or PIR_sensor1.get_status() or PIR_sensor2.get_status() 
          ){
     
     //a fish was detected!
@@ -118,6 +124,6 @@ void initiate()
 	//Start Serial communication for debugging.
 	Serial.begin(9600);
 	Serial.println("Connected");
-	Serial.println("Arduino firmware 20220728_16h53");
+	Serial.println("Arduino firmware 20220729_16h37");
 	
 }
